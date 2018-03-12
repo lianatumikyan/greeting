226d69c9589bd3cbcf3b5060a32cd657dcb4cb8d
Liana Tumikyan <ltumikyan@gmail.com>
2018-03-12 20:46:53 +0200

    use es6 functionality & small fixings

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
 'use strict';
 (function () {
 
- function greeting (greeting) {
-        var greetingWord = greeting;
-        return function greeting (name) {
-            return (greetingWord + ' ' +  name);
+    class Init {
+        constructor() {
+            let variables = {
+                hello: null,
+                type: $('.greetingType'),
+                name: $('.name'),
+                greetVal: $('#greeting'),
+                nameVal: $('#name'),
+                centered: $('.centered')
+            };
+            if (variables.type){
+                variables.type.on("submit", e => this.submitType(e, variables));
+            }
+            if (variables.name){
+                variables.name.on("submit", e => this.submitName(e, variables));
+            }
+
+        }
+
+        submitType(e, variables) {
+            e.preventDefault();
+            variables.hello = this.greeting(variables.greetVal.val());
+            variables.type.fadeOut();
+            variables.name.fadeIn();
+        }
+
+        submitName(e, variables) {
+            e.preventDefault();
+            let text = variables.hello(variables.nameVal.val());
+            variables.centered.text(text);
+        }
+
+
+        greeting (greeting) {
+            return function (name) {
+                return (greeting + ' ' +  name);
+            }
         }
     }
 
-    function init() {
-     var hello = null;
-
-        $('.greetingType').on('submit', function (e) {
-            e.preventDefault();
-            hello = greeting($('#greeting').val());
-            $('.greetingType').fadeOut();
-            $('.name').fadeIn();
-        });
-
-        $('.name').on('submit', function (e) {
-            e.preventDefault();
-            var text = hello($('#name').val())
-            $('.centered').text(text);
-        })
-    }
-
-    init();
+    new Init();
 })();
-
-
-
-
